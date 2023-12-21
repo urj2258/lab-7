0cfe0b10df15842f9bde3e091c145ed34507ed0b
urj2258 <liaqtumer296@gmail.com>
2023-12-22 02:41:38 +0500

    initializing sum adding a for loop for sum of array

--- exercises/array_utils.c
+++ exercises/array_utils.c
@@ -66,6 +66,17 @@ double getMean(const int *arr, int size) {
   {
     return 0.0;
   }
+
+double sum=0.0;
+
+  for(int i=0;i<size;i++)
+  {
+    sum+=arr[i];
+  }
+
+  return sum/size;
+  
+  
 }
 
 int getMin(const int *arr, int size) {
